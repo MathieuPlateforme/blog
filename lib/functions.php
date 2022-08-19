4633ef0687045c3f77757288d7bf2fc542e58e6e
MathieuPlateforme <mathieu.ruiz@laplateforme.io>
2022-08-19 23:51:55 +0200

    Merci StackOverflow

--- lib/functions.php
+++ lib/functions.php
@@ -108,3 +108,11 @@ function deleteContent($table, $id)
     $stmt->execute();
     
 }
+function limit_text($text, $limit) {
+    if (str_word_count($text, 0) > $limit) {
+        $words = str_word_count($text, 2);
+        $pos   = array_keys($words);
+        $text  = substr($text, 0, $pos[$limit]) . '...';
+    }
+    return $text;
+}
