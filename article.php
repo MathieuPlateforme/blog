3c6e24d72c280786fd05f8d798c30a4e3dbf5a8d
MathieuPlateforme <mathieu.ruiz@laplateforme.io>
2022-08-19 23:43:34 +0200

    Page Article fait

---
+++ article.php
@@ -0,0 +1,22 @@
+<?php
+session_start();
+include("lib/db.php");
+include("lib/functions.php");
+include("models/user.php");
+include("models/articles.php");
+
+$view = "article";
+
+if(array_key_exists('id', $_GET))
+{
+    $id = $_GET['id'];
+    $data = getContent('articles', $id);
+    $idCategory = $data['id_categorie'];
+    $idUtilisateur = $data['id_utilisateur'];
+}
+else
+{
+    $data = listContent('articles', 5);
+}
+
+include("tpl/layout.phtml");
